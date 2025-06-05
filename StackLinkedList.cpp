9be22f105f22176e5fe8aa1be52d08b8873a1cad
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-05 17:33:58 +0700

    lanjutan fungsi stack

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -27,6 +27,11 @@ class Stack
         int push(int value)
         {
             Node *newNode = new Node();
+            newNode->data = value;
+            newNode->next = top;
+            top = newNode;
+            cout << "Push Value : " << value << endl;
+            return value;
         }
 };
 
