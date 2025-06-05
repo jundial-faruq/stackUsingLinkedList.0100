3db2b7cbcec219de183484b3fac5348beb812170
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-05 17:39:53 +0700

    membuat fungsi pop

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -26,13 +26,19 @@ class Stack
         //push operation insert an element onto the top of the top
         int push(int value)
         {
-            Node *newNode = new Node();
-            newNode->data = value;
-            newNode->next = top;
-            top = newNode;
-            cout << "Push Value : " << value << endl;
+            Node *newNode = new Node();  // 1. Allocate memory
+            newNode->data = value; // 2. assign value
+            newNode->next = top; // 3. set next point
+            top = newNode; // 4. update the top point
+            cout << "Push Value : " << value << endl; //
             return value;
         }
+    public :
+        void pop()
+        {
+         
+        }
+
 };
 
 int main()
