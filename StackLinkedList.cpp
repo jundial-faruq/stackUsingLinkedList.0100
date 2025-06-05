d3c2dd8e0275cea490ba1b139f8a7eb0e982918e
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-05 18:04:39 +0700

    membuat cout nya dan cin

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -85,7 +85,13 @@ int main()
 
     while (choice != 5)
     {
- 
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
     
     return 0;
 }
