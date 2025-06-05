5234446edcfcf4ac767e3e01150d2753c3d05b70
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-05 17:31:06 +0700

    membuat fungsi stack

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -18,7 +18,16 @@ class Stack
     private :
         Node *top;// Pointer to the top node of the stack
 
-    
+    public :
+        Stack()
+        {
+            top = NULL;//Initialize the stackwith a null top
+        }
+        //push operation insert an element onto the top of the top
+        int push(int value)
+        {
+            Node *newNode = new Node();
+        }
 };
 
 int main()
